e053a29c6ae5b4d088e019204b3efcf94b3c072a
nobita <nobitagroup001@gmail.com>
2024-07-15 20:16:39 +0530

    add video model

--- project03-videoTube/src/controllers/user.controller.js
+++ project03-videoTube/src/controllers/user.controller.js
@@ -343,7 +343,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
       $lookup : {
         from : "subscriptions",
         localField : "_id",
-        foreignField : "subscribers",
+        foreignField : "subscriber",
         as : subscribedTo
       }
     },
